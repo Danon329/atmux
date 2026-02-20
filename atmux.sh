fa9004d285a34e9c2fe0556b5c5070d9d3893255
Daniel Feldman <daniel.salit@hotmail.com>
2026-02-20 20:07:34 +0100

    Initial Commit; Initial checks for bash file, basic functionality of manager and saving to json

---
+++ atmux.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+exec 200>/tmp/atmux.lock
+
+MANUAL_CREATION=false
+SESSION_NAME="0"
+
+SESSION_PATH=$PWD
+NUMBER_WINDOWS=1
+declare -a WINDOW_NAMES # that should create an Array I think
+
+# get option flags
+while getopts "s:m" opts; do
+	case $opts in
+		s)
+			SESSION_NAME=$OPTARG
+			;;
+		m)
+			MANUAL_CREATION=true
+			;;
+	esac
+done
+
+# TODO: Get running processes for tmux, get names, set names running true, every other session false
+
+# Start file lock
+flock 200
+
+# Get tmux sessions
+EXISTING_SESSIONS=$(tmux list-sessions -F "#{session_name} #{session_attached}" 2>dev/null)
+
+# Look whether there even is an active tmux server
+if [ -z "$EXISTING_SESSIONS "]; then
+	python3 session_manager.py set_all_running_false
+else
+	PRESUMED_RUNNING=$(python3 session_manager.py get-running-all)
+	read -r -a PRESUMED_RUNNING_ITEMS <<< "$PRESUMED_RUNNING"
+
+	# Check whether in save there are running sessions that don't even exist in the server
+	for PRESUMED_RUNNING_ITEM in "${PRESUMED_RUNNING_ITEMS[@]}"; do
+		if [[ " $EXISTING_SESSIONS " != *" $PRESUMED_RUNNING_ITEM "* ]]; then
+			python3 session_manager.py set_running $PRESUMED_RUNNING_ITEM False
+		fi
+	done
+
+	# Update the running and non-running sessions in file against actual tmux server
+	while read -r EXISTING_SESSION_NAME IS_RUNNING; do
+		if ["$IS_RUNNING" -gt 0]; then
+			python3 session_manager.py set_running $EXISTING_SESSION_NAME True
+		else
+			python3 session_manager.py set_running $EXISTING_SESSION_NAME False
+		fi
+	done <<< "$EXISTING_SESSIONS" # feeds the data into the loop
+fi
+
+# TODO: Now think about attachment or creation of needed session
+
+# Check whether Session already exists
+TMUX_HAS_SESSION=$(python3 session_manager.py "check" $SESSION_NAME)
+IS_SESSION_RUNNING=$(python3 session_manager.py "get-running" $SESSION_NAME)
+
+if ["$TMUX_HAS_SESSION" = "True"]; then
+	if ["$IS_SESSION_RUNNING" = "True"]; then
+		echo "Session is already running"
+		echo "Hope you called through atmux for running control"
+	else
+		# TODO:
+		# attach tmux (maybe in other window)
+		# rename own window, to close own window after use
+		# get running threads, check for tmux windows
+		# Think about how to know, when tmux window is closed
+	fi
+fi
+
+# TODO:
+# if manual creation needed, get extra info
+
---
+++ session_manager.py
@@ -0,0 +1,126 @@
+import sys
+import json
+import os
+
+SESSIONS_FILE: str = "sessions.json"
+
+
+def load_sessions() -> dict:
+    if not os.path.exists(SESSIONS_FILE):
+        return {}
+
+    with open(SESSIONS_FILE, "r") as file:
+        try:
+            return json.load(file)
+        except json.JSONDecodeError:
+            return {}
+
+
+def save_sessions(hashmap: dict):
+    with open(SESSIONS_FILE, "w") as file:
+        json.dump(hashmap, file, indent=4)
+
+
+def check_session_exists(session_name: str) -> bool:
+    sessions_map: dict = load_sessions()
+
+    if sessions_map.get(session_name) is not None:
+        return True
+    else:
+        return False
+
+
+def set_session(
+    session_name: str,
+    session_path: str,
+    window_count: int,
+    window_names: dict,
+    is_session_running: str,
+):
+    session_map: dict = load_sessions()
+    session_details: dict = {
+        "session_path": session_path,
+        "window_count": window_count,
+        "window_names": window_names,
+        "is_session_running": is_session_running,
+    }
+
+    session_map[session_name] = session_details
+    save_sessions(session_map)
+
+
+def get_session(session_name: str) -> str:
+    session_map: dict = load_sessions()
+    return session_map[session_name]
+
+
+def get_session_running(session_name: str) -> str:
+    session_map: dict = load_sessions()
+    return session_map[session_name]["is_session_running"]
+
+
+def set_session_running(session_name: str, session_running: str):
+    session_map: dict = load_sessions()
+    session_map[session_name]["is_session_running"] = session_running
+
+
+def main():
+    # Args: 0: script_name, 1... etc
+    command = sys.argv[1]
+
+    session_name = None
+    session_path = None
+    window_count = None
+    is_session_running = None
+    window_names = {}
+
+    match command:
+        case "check":
+            session_name = sys.argv[2]
+            print(check_session_exists(session_name))
+        case "set":
+            session_name = sys.argv[2]
+
+            # always give path, but don't need to always give window_count
+            if len(sys.argv) > 4:
+                session_path = sys.argv[3]
+                window_count = int(sys.argv[4])
+
+                for i in range(window_count):
+                    window_names[i + 1] = sys.argv[5 + i]
+            elif len(sys.argv) == 4:
+                session_path = sys.argv[3]
+                window_count = 1
+                window_names = {1: "zsh"}
+            else:
+                session_path = "~"
+                window_count = 1
+                window_names = {1: "zsh"}
+
+            set_session(
+                session_name,
+                session_path,
+                window_count,
+                window_names,
+                "False",  # set running always false, handle it manually through extra call
+            )
+        case "get":
+            session_name = sys.argv[2]
+            # TODO: need specific gets. Cannot just parse a dict to bash (I am not gifted enough for that)
+        case "get-running-all":
+            # TODO: get all running sessions with running = True. Give them back in one string --format: "session1 session2 session3"
+            pass
+        case "get-running":
+            session_name = sys.argv[2]
+            print(get_session_running(session_name))
+        case "set-running":
+            session_name = sys.argv[2]
+            is_session_running = sys.argv[3]
+            set_session_running(session_name, is_session_running)
+        case "set_all_running_false":
+            # TODO: Write a function that iterates through all sessions and sets them false
+            pass
+
+
+if __name__ == "__main__":
+    main()
